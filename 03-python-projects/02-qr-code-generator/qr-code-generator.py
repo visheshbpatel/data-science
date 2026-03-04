f93cb56f220824256a6f39ed9ad88b765435c78e
visheshbpatel <iamvisheshpatel@gmail.com>
2026-03-05 01:03:21 +0530

    Added fucntions for each task

--- 03-python-projects/02-qr-code-generator/qr-code-generator.py
+++ 03-python-projects/02-qr-code-generator/qr-code-generator.py
@@ -1,11 +1,40 @@
-import qrcode
+import qrcode as qr
 from pyzbar.pyzbar import decode
 from PIL import Image
 
-qr_img = qrcode.make("https://github.com/visheshbpatel")
 
-qr_img.save("qr-img.png", scale=10)
+def generate_qr(data, filename="qr-img.png"):
+    qr_code = qr.make(data)
+    qr_code.save(filename)
+    return filename
 
 
-d = decode(Image.open("qr-img.png"))
-print(d[0].data.decode("ascii"))
+def decode_qr(filename):
+    
+    img = Image.open(filename)
+    decoded_data = decode(img)
+
+    if not decoded_data:
+        return None
+    
+    return decoded_data[0].data.decode("ascii")
+
+
+def main():
+    url = "https://github.com/visheshbpatel"
+
+    file = generate_qr(url)
+    print("QR Code generated successfully")
+    print("Saved as:", file)
+
+    decode_result = decode_qr(file)
+
+    if decode_result:
+        print("Decode Data:",decode_result)
+    else:
+        print("No QR code Found!!!")
+
+
+
+if __name__ == "__main__":
+    main()
