d713b3d5412ebe63ec1fd7ff8a22246e71e6dcf8
visheshbpatel <iamvisheshpatel@gmail.com>
2026-03-05 10:26:41 +0530

    replace pyzbar and PIL with OpenCV and os

--- 03-python-projects/02-qr-code-generator/qr-code-generator.py
+++ 03-python-projects/02-qr-code-generator/qr-code-generator.py
@@ -1,36 +1,91 @@
-import qrcode as qr
-from pyzbar.pyzbar import decode
-from PIL import Image
+import qrcode
+import cv2
+import os
 
 
+# Generate qr code
 def generate_qr(data, filename="qr-img.png"):
-    qr_code = qr.make(data)
-    qr_code.save(filename)
-    return filename
+
+    """
+    Generate a QR Code for the give data
+    """
+
+    try:
+
+        if not data:
+            raise ValueError("Data Cannot be Empty")
+        qr = qrcode.QRCode(
+            version=2,
+            box_size=10,
+            border=5
+        )
+        
+        qr.add_data(data)           
+        qr.make(fit=True)           # if box size small for the give output automatically increase it
+
+        img = qr.make_image(fill_color="Black", back_color="white")
+        img.save(filename)
 
 
-def decode_qr(filename):
-    
-    img = Image.open(filename)
-    decoded_data = decode(img)
+        return filename
 
-    if not decoded_data:
+    except Exception as e:
+        print("Error Generating QR code",e)
         return None
     
-    return decoded_data[0].data.decode("ascii")
+
+# show qr code dirrectly on screen
+def show_qr(file):
+
+    path = os.path.abspath(file)
+    os.startfile(path)
+
+    
+
+# Decode the qr code
+def decode_qr(filename):
+
+    """"
+    Decode Qr code using OpenCV
+    """
+
+    img = cv2.imread(filename)
+    
+    if img is None:
+        print("Image Not Found")
+        return None
+    
+    decoder = cv2.QRCodeDetector()
+
+    data, bbox, _ = decoder.detectAndDecode(img)
+
+
+    # returning the generated image
+    if bbox is not None:
+        return data
+
+    return  None
+
 
 
 def main():
-    url = "https://github.com/visheshbpatel"
 
-    file = generate_qr(url)
+    data = input("Enter the data or URL to generate QR Code:")
+    filename = input("Enter the file name of QR code you want: ").lower().strip()
+    if not filename.endswith(".png"):
+        filename+=".png"
+
+    file = generate_qr(data , filename)
     print("QR Code generated successfully")
     print("Saved as:", file)
+    print("Saved at:", os.path.abspath(file))
+    if file:
+        show_qr(file)
 
-    decode_result = decode_qr(file)
 
-    if decode_result:
-        print("Decode Data:",decode_result)
+    result = decode_qr(file)
+    if result:
+        print("Decode Data:",result)
     else:
         print("No QR code Found!!!")
 
