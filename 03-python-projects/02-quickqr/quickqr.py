cc17c017c8ed8a52226bddfafc55029b10f77af5
visheshbpatel <iamvisheshpatel@gmail.com>
2026-03-06 17:59:34 +0530

    Add CLI improvements to QuickQR

--- 03-python-projects/02-quickqr/quickqr.py
+++ 03-python-projects/02-quickqr/quickqr.py
@@ -9,13 +9,14 @@ import webbrowser
 def generate_qr(data, filename="qr-img.png"):
 
     """
-    Generate a QR Code for the give data
+    Generate a QR Code for the given data
     """
 
     try:
 
         if not data:
             raise ValueError("Data Cannot be Empty")
+        
         qr = qrcode.QRCode(
             version=2,
             box_size=10,
@@ -25,7 +26,7 @@ def generate_qr(data, filename="qr-img.png"):
         qr.add_data(data)           
         qr.make(fit=True)           # if box size small for the give output automatically increase it
 
-        img = qr.make_image(fill_color="Black", back_color="white")
+        img = qr.make_image(fill_color="black", back_color="white")
         img.save(filename)
 
 
@@ -63,7 +64,7 @@ def decode_qr(filename):
 
 
     # returning the generated image
-    if bbox is not None:
+    if bbox is not None and data:
         return data
 
     return  None
@@ -80,21 +81,24 @@ def scan_qr_camera():
     print("press 'o' to open link")
     print("press 'q' to quit")
 
-    while True:
+    try:
+        while True:
 
-        ret, frame = cap.read()
+            ret, frame = cap.read()
 
-        if not ret:
-            continue
+            if not ret:
+                continue
 
-        data, bbox, _ = detector.detectAndDecode(frame)
+            data, bbox, _ = detector.detectAndDecode(frame)
 
-        if bbox is not None:
+            qr_data = None
 
-            for i in range(len(bbox)):
-                pt1 = tuple(bbox[i][0].astype(int))
-                pt2 = tuple(bbox[(i+1) % len(bbox)][0].astype(int))
-                cv2.line(frame, pt1, pt2, (0,255,0), 2)
+            if bbox is not None:
+
+                for i in range(len(bbox)):
+                    pt1 = tuple(bbox[i][0].astype(int))
+                    pt2 = tuple(bbox[(i+1) % len(bbox)][0].astype(int))
+                    cv2.line(frame, pt1, pt2, (0,255,0), 2)
 
                 if data:
                     qr_data = data
@@ -117,48 +121,90 @@ def scan_qr_camera():
                             0.9,
                             (0,255,0),
                             2)
-                
-        cv2.imshow("QR Scanner", frame)
+                    
+            cv2.imshow("QR Scanner", frame)
 
-        key = cv2.waitKey(1)
+            key = cv2.waitKey(1)
 
-        if key==ord("q"):
-            break
-        
-        if key==ord("o") and qr_data:
-            try:
-                print("opening: ", qr_data)
-                webbrowser.open(qr_data)
-            except:
-                print("No Link present")
+            if key==ord("q"):
+                break
+            
+            if key==ord("o") and qr_data:
+                try:
+                    print("opening: ", qr_data)
+                    webbrowser.open(qr_data)
+                except:
+                    print("No Link present")
 
-    cap.release()
-    cv2.destroyAllWindows()
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
 
 
 
 def main():
 
-    # data = input("Enter the data or URL to generate QR Code:")
-    # filename = input("Enter the file name of QR code you want: ").lower().strip()
-    # if not filename.endswith(".png"):
-    #     filename+=".png"
 
-    # file = generate_qr(data , filename)
-    # print("QR Code generated successfully")
-    # print("Saved as:", file)
-    # print("Saved at:", os.path.abspath(file))
-    # if file:
-    #     show_qr(file)
+    while True:
+
+        os.system("cls")
+        
+        print("\n=== QR Code Tools ===")
+        print("1. Generate QR code")
+        print("2. Decode QR Code")
+        print("3. Scan QR Code")
+        print("4. Exit")
+
+        choice = input("Ente your choice: ")
+
+        if choice=='1':
+
+            data = input("Enter the data or URL to generate QR Code:")
+            filename = input("Enter the file name of QR code you want: ").lower().strip()
+            if not filename.endswith(".png"):
+                filename+=".png"
 
 
-    # result = decode_qr(file)
-    # if result:
-    #     print("Decode Data:",result)
-    # else:
-    #     print("No QR code Found!!!")
+            file = generate_qr(data , filename)
+            if file:
+                print("QR Code generated successfully")
+                print("Saved as:", file)
+                print("Saved at:", os.path.abspath(file))
+                show_qr(file)
 
-    scan_qr_camera()
+            else:
+                print("QR generation failed")
+
+            input("\nPress Enter to continue...")
+
+        elif choice=='2':
+
+            file_path = input("Enter Qr File path: ")
+
+            result = decode_qr(file_path)
+            if result:
+                print("Decode Data:",result)
+            else:
+                print("No QR code Found!!!")
+
+            input("\nPress Enter to continue...")
+        
+        elif choice=='3':
+
+            scan_qr_camera()
+
+            input("\nPress Enter to continue...")
+        
+        elif choice=='4':
+
+            print("Thanks for using the service")
+            print("Exiting the program...")
+            break
+
+        else:
+            print("Invalid Choice")
+            print("Try Again...")
+            input("\nPress Enter to continue...")
 
 
 
