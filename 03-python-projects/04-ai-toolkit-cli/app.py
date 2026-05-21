7459924524177ad69879c7c273be47088948329a
visheshbpatel <iamvisheshpatel@gmail.com>
2026-05-21 14:58:58 +0530

    add argparse code

--- 03-python-projects/04-ai-toolkit-cli/app.py
+++ 03-python-projects/04-ai-toolkit-cli/app.py
@@ -1,13 +1,34 @@
 import os
+import argparse
 from openai import OpenAI
 from dotenv import load_dotenv
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("task")
+parser.add_argument("text")
+
+args = parser.parse_args()
+
+
+
+if args.task == "summarize":
+    prompt = f"Summerize this text: {args.text}"
+
+elif args.task == "translate":
+    prompt = f"translate this text: {args.text}"
+
+elif args.task == "sentiment-analysis":
+    prompt = f"sentiment analysis on this text: {args.text}"
+
+else:
+    print("Please provide valid task or Text")
 
 
 load_dotenv()
 
-api_key = os.getenv("API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
 base_url = os.getenv("BASE_URL")
-model = os.getenv("MODEL")
 
 client = OpenAI(
     api_key = api_key,
@@ -15,11 +36,11 @@ client = OpenAI(
 
 
 response = client.chat.completions.create(
-    model=model,
+    model="llama-3.3-70b-versatile",
     messages=[
         {
             "role":'user',
-            "content": "Hello AI"
+            "content": prompt
         }
     ]
 )
