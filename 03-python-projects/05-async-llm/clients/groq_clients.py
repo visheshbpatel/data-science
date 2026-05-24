6d61e50e8f9064bdf1164ad664ba7b5c22eafec9
visheshbpatel <iamvisheshpatel@gmail.com>
2026-05-24 22:46:49 +0530

    add groq  client file

---
+++ 03-python-projects/05-async-llm/clients/groq_clients.py
@@ -0,0 +1,48 @@
+import httpx
+from utils.config import(groq_api_key, groq_base_url, groq_model)
+
+async def ask_groq(prompt):
+
+    url = f"{groq_base_url}/chat/completions"
+
+    headers = {
+        "Authorization": f"Bearer {groq_api_key}",
+        "Content-Type": "application/json"
+    }
+
+    json_data = {
+        "model": groq_model,
+
+        "messages": [
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    }
+
+    print(url)
+
+    async with httpx.AsyncClient() as client:
+
+        response = await client.post(
+            url,
+            headers=headers,
+            json=json_data
+        )
+
+        if response.status_code != 200:
+
+            print("Request Failed")
+            print(response.json())
+
+            return
+
+        data = response.json()
+
+        content = data['choices'][0]['message']['content']
+    
+        return {
+            "provider": "Groq",
+            "response": content
+        }
