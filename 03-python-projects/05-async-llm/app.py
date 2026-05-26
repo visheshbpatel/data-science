d76a86e63696609d6ab8d8ec1fcbf9e7f8b33077
visheshbpatel <iamvisheshpatel@gmail.com>
2026-05-26 08:58:34 +0530

    update the main for result

--- 03-python-projects/05-async-llm/app.py
+++ 03-python-projects/05-async-llm/app.py
@@ -2,16 +2,19 @@ import asyncio
 
 from clients.groq_clients import ask_groq
 from clients.gemini_clients import ask_gemini
+from clients.openrouter_clients import ask_openrouter
 from utils.display import display_results
 
 
 async def main():
 
-    prompt = "Explain AI in 2 lines"
+    prompt = "Why is async programming useful in Python?"
 
     results = await asyncio.gather(
         ask_groq(prompt),
-        ask_gemini(prompt)
+        ask_gemini(prompt),
+        ask_openrouter(prompt),
+        return_exceptions=True
     )
 
     display_results(results)
